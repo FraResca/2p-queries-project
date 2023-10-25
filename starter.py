6d11d3dd8fef70c542b039bce2d39c282c819046
FraResca <francesco.resca@edu.unife.it>
2023-10-25 15:34:48 +0200

    more exps

--- starter.py
+++ starter.py
@@ -2,7 +2,11 @@ import os.path as osp
 from kbc.cqd_beam import *
 
 
-model_path = "models/FB15k-model-rank-100-epoch-100-1602511249.pt"
+#model_path = "models/FB15k-model-rank-100-epoch-100-1602511249.pt"
+model_paths = ["models/FB15k-model-rank-100-epoch-100-1602511249.pt",
+			   "models/FB15k-model-rank-200-epoch-100-1602513660.pt",
+			   "models/FB15k-model-rank-500-epoch-100-1602516425.pt",
+			   "models/FB15k-model-rank-1000-epoch-100-1602520745.pt"]
 path = "data/FB15k"
 dataset = osp.basename(path)
 mode = "test"
@@ -16,7 +20,8 @@ data_complete_path = osp.join(path, f'{dataset}_{mode}_complete.pkl')
 data_hard = pickle.load(open(data_hard_path, 'rb'))
 data_complete = pickle.load(open(data_complete_path, 'rb'))
 
-run(model_path, data_hard, data_complete,
-	dataset, t_norm, candidates,
-	scores_normalize,
-	kg_path=path, explain=False)
+for model_path in model_paths:
+	run(model_path, data_hard, data_complete,
+		dataset, t_norm, candidates,
+		scores_normalize,
+		kg_path=path, explain=False)
